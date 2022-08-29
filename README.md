5205bd08bc9d9c2fa3bf72243fc4ce0cb2471fec
Mm1KEE <MIKE.CHEN53@GMAIL.COM>
2022-08-29 22:57:39 +0800

    add files

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ClosestDataAverage
-Simple denoising algorithm for my touch library.Based on the "Averaging the Closest Data" in this doc by Wendy Fang from TI.
+Simple denoising algorithm for my touch library , which (in theory) could also be used to process other raw inputs.Based on the "Averaging the Closest Data" in this doc by Wendy Fang from TI.
 https://www.ti.com/lit/an/sbaa155a/sbaa155a.pdf
+# Usage
+Create one (or two) array containing 3 integers.
+
+```
+int xArray[3] = { -16, 3, 1};
+int yArray[3] = { -126, 2, 2 };
+```
+
+Pass your int array to this library , use `getXAvg()` and `getYAvg()` to get return value.
+
+```
+int xAvg=getXAvg();
+int yAvg=getYAvg();
+```
+
+Above example will get a result of `2` and `2`.
---
+++ examples/AveragingClosestDataExample/AveragingClosestDataExample.ino
@@ -0,0 +1,40 @@
+/*
+   @Author: Mm1KEE
+   @GitHub: https://github.com/Mm1KEE/ClosestDataAverage
+   @Date: 2022-08-29 22:24:24
+   @LastEditTime: 2022-08-29 22:32:07
+   @Description: This demonstrates shows how to use ClosestDataAverage library.
+
+   Copyright (c) 2022 by Mm1KEE, All Rights Reserved.
+*/
+#include <ClosestDataAverage.h>
+int xArray[3] = { -16, 3, 1};
+int yArray[3] = { -126, 2, 2 };
+int xAvg, yAvg;
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  //pass the array
+  rawDataFilter(xArray, yArray);
+  //get results
+  xAvg = getXAvg();
+  yAvg = getYAvg();
+  Serial.println("averaged x:" + String(xAvg));
+  Serial.println("averaged y:" + String(yAvg));
+}
+
+void loop() {
+  //read int form source,eg,serial,anolog inputs
+  Serial.println("Input 3 integers for process.");
+  for (int i = 0; i <= 2; i++) {
+    while (!Serial.available());
+    //store int in the array
+    xArray[i] = Serial.parseInt(SKIP_ALL,10);
+    Serial.println("Read int " + String(i) + " :" + String(xArray[i]));
+  }
+  //process the array
+  rawDataFilter(xArray, yArray);
+  //get result
+  xAvg = getXAvg();
+  Serial.println("Closest Average of above numbers is :" + String(xAvg));
+}
